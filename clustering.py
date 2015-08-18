d050e3781c127787e4e897668328fb77e05a3411
Andrew Gartland <agartland@gmail.com>
2015-08-18 12:53:06 -0700

    Generalized clustering functions.

--- clustering.py
+++ clustering.py
@@ -1,95 +1,99 @@
 import scipy.cluster.hierarchy as sch
 import itertools
-import palettable
 
-from corrplots import *
-from hclusterplot import *
-from gapstat import *
-from bootstrap_cluster import *
+import scipy.spatial.distance as distance
+import scipy.cluster.hierarchy as sch
 
-'''
-method = 'complete' #single, centroid (UPGMC), complete, average, ward
-metric = 'pearson-signed'
-minPatients = 30
-K = 6
-tiss = 'ETT'
-dmatFunc = partial(computeDMat, metric = metric, minN = minPatients)
-clusterFunc = partial(clusterFuncK, K = K, method = method)
+from gapstat import computeGapStat
+from bootstrap_cluster import bootstrapFeatures, bootstrapObservations
 
-"""Use the gap statistic to determine the number of clusters"""
-for normStr,tmpDf in zip(['normed','raw'],(ndf,df)):
-    for tiss in ['ETT','Serum']:
-        gs = computeGapStat(tmpDf[cyVars[tiss]], dmatFunc, partial(clusterFuncK,method = method), maxK = 15, bootstraps = 20)
-        figure(50, figsize = (10,8.5))
-        plotGapStat(*gs)
-        figure(50).savefig(DATA_PATH + 'RandolphFlu/figures/PIC_%s_%s_gap_statistic.png' % (tiss,normStr))
+__all__ = ['hierClusterFunc',
+           'gmmClusterFunc',
+           'corrDmatFunc',
+           'makeModuleVariables'
+           'formReliableClusters']
 
-"""Use bootstrap_clustering to determine the reliable clusters in PIC"""
-modulesPICN = {}
-modulesPIC = {}
-pwrelPICN = {}
-pwrelPIC = {}
-for tiss in ['ETT','Serum']: 
-    pwrelPICN[tiss], modulesPICN[tiss] = findReliableClusters(ndf, tiss, cyVars, dmatFunc, clusterFunc, 'PIC_normed')
-    pwrelPIC[tiss], modulesPIC[tiss] = findReliableClusters(df, tiss, cyVars, dmatFunc, clusterFunc, 'PIC')
+def corrDmatFunc(cyDf, metric = 'pearson-signed', dfunc = None, minN = 30):
+    if dfunc is None:
+        if metric in ['spearman', 'pearson']:
+            """Anti-correlations are also considered as high similarity and will cluster together"""
+            dmat = (1 - cyDf.corr(method = metric, min_periods = minN).values**2).values
+            dmat[np.isnan(dmat)] = 1
+        elif metric in ['spearman-signed', 'pearson-signed']:
+            """Anti-correlations are considered as dissimilar and will NOT cluster together"""
+            dmat = ((1 - cyDf.corr(method = metric.replace('-signed',''), min_periods = minN).values) / 2).values
+            dmat[np.isnan(dmat)] = 1
+        else:
+            raise NameError('metric name not recognized')
+    else:
+        ncols = cyDf.shape[1]
+        dmat = np.zeros((ncols, ncols))
+        for i in range(ncols):
+            for j in range(ncols):
+                """Assume distance is symetric"""
+                if i <= j:
+                    tmpdf = cyDf.iloc[:,[i,j]]
+                    tmpdf = tmpdf.dropna()
+                    if tmpdf.shape[0] >= minN:
+                        d = dfunc(cyDf.iloc[:,i],cyDf.iloc[:,j])
+                    else:
+                        d = np.nan
+                    dmat[i,j] = d
+                    dmat[j,i] = d
+    return pd.DataFrame(dmat, columns = cyDf.columns, index = cyDf.columns)
 
-modndf = makeModuleVariables(ndf, modulesPICN)
-moddf = makeModuleVariables(df, modulesPICN)
-
-corrPlotsByCluster(ndf, modulesPICN,'PIC_normed')
-corrPlotsByCluster(df, modulesPIC,'PIC')
-
-for tiss in ['ETT','Serum']:
-    plotModuleEmbedding(ndf, tiss, modulesPICN, dmatFunc, 'PIC_normed')
-    plotModuleEmbedding(df, tiss, modulesPIC, dmatFunc, 'PIC')
-'''
-
-def clusterFuncK(dmat, K = 6, method = 'complete'):
-    hclusters = sch.linkage(dmat, method = method)
-    labels = sch.fcluster(hclusters, K, criterion = 'maxclust')
+def hierClusterFunc(dmatDf, K = 6, method = 'complete'):
+    hclusters = sch.linkage(dmatDf.values, method = method)
+    labelsVec = sch.fcluster(hclusters, K, criterion = 'maxclust')
+    labels = pd.Series(labelsVec, index = dmatDf.columns)
     return labels
 
-def findReliableClusters(df, tiss, cyVars, dmatFunc, clusterFunc, studyStr, bootstraps=500, threshold=0.5):
-    """Use bootstrap_clustering to determine the reliable clusters in PIC"""
+def gmmClusterFunc(dmatDf, K = 6):
+    """Soft clustering in high-dimensions, with constraints"""
+    pass
+
+def findReliableClusters(cyDf, dmatFunc, clusterFunc, bootstraps = 500, threshold = 0.5):
+    """Use bootstrap_clustering to determine the reliable clusters"""
     clusters = {}
-    dmat = dmatFunc(df[cyVars[tiss]])
+    dmatDf = dmatFunc(cyDf)
     #pwrel, labels = bootstrapFeatures(dmat, clusterFunc, bootstraps = bootstraps)
-    pwrel, labels = bootstrapObservations(df[cyVars[tiss]], dmatFunc, clusterFunc, bootstraps = bootstraps)
-    pwrel = pd.DataFrame(pwrel, index = cyVars[tiss], columns = cyVars[tiss])
-    labels = pd.Series(labels, index = cyVars[tiss])
-
-    pwrelPlot = pwrel.copy()
-
+    pwrelDf, labels = bootstrapObservations(cyDf, dmatFunc, clusterFunc, bootstraps = bootstraps)
+    
+    dropped = pd.Series(np.zeros((cyDf.shape[1],cyDf.shape[1])).astype(bool), index = cyDf.columns)
     for currLab in labels.unique():
-        cyMembers = labels.index[labels == currLab]
+        cyMembers = labels.index[labels == currLab].tolist()
         """Step-down: start with all members and discard fringe"""
-        clusters[currLab] = cyMembers.tolist()
         for cy in cyMembers:
-            if pwrel[cy].loc[cyMembers].drop(cy).mean() > threshold:
-                clusters[currLab].remove(cy)
-                pwrelPlot = pwrelPlot.rename_axis({cy: '*' + cy}, axis = 1)
-                print 'Excluded %s from %s cluster %s: mean reliability is %1.0f%%' % (cy,tiss,currLab,100*(1-pwrel[cy].loc[cyMembers].drop(cy).mean()))
+            meanReliability = (1 - pwrelDf[cy].loc[cyMembers].drop(cy).mean())
+            if  meanReliability < threshold:
+                dropped[cy] = True
+                print 'Excluded %s from cluster %s: mean reliability was %1.0f%%' % (cy, currLab, 100 * meanReliability)
         
-        """Step-up strategy: start with best and add those that fit"""
-        '''
-        clusters[currLab] = []
-        """Best is the cytokine among the cluster that is closest to all other members"""
-        bestCy = pwrel[cyMembers].loc[cyMembers].sum(axis=0).argmin()
-        prntTuple = (bestCy,tiss,currLab,100*(1-(pwrel[cyMembers].loc[cyMembers].mean(axis=0).min())), (100*(1-pwrel[cyMembers].loc[cyMembers].mean(axis=0))).tolist())
-        print '%s is best from %s cluster %s: mean reliability is %1.0f%% (%s)' % prntTuple
-        clusters[currLab].append(bestCy)
-        for cy in cyMembers.drop(bestCy):
-            currMembers = clusters[currLab]
-            if (pwrel[cy].loc[currMembers]<threshold).any():
-                clusters[currLab].append(cy)
-            else:
-                print 'Excluded %s from %s cluster %s: max reliability is %1.0f%%' % (cy,tiss,currLab,100*(1-(pwrel[cy].loc[currMembers]).min()))
-        '''
-        if len(clusters[currLab]) <= 1:
-            print 'Dropped cluster %s %s' % (tiss,currLab)
-            popped = clusters.pop(currLab)
+        """Consider step-up strategy: start with best and add those that fit"""
+    return pwrelDf, labels, dropped
 
-    figure(641, figsize = (15.5, 9.5))
-    colInds = plotHColCluster(df = None, col_dmat = pwrelPlot, col_labels = labels, titleStr = studyStr)
-    figure(641).savefig(DATA_PATH + 'RandolphFlu/figures/%s_%s_cluster_reliability.png' % (studyStr, tiss))
-    return pwrel, clusters
+def labels2modules(labels, dropped = None):
+    uLabels = np.unique(labels)
+    out = {lab:labels.index[labels == lab].tolist() for lab in uLabels}
+    if not dropped is None:
+        todrop = dropped.index[dropped].tolist()
+        for lab in out.keys():
+            out[lab] = [cy for cy in out[lab] if not cy in todrop]
+    return out
+
+def makeModuleVariables(cyDf, labels, dropped = None):
+    """Define variable for each module by standardizing all the cytokines in the module and taking the mean"""
+    if dropped is None:
+        dropped = pd.Series(np.ones((labels.shape[0])), index = labels.index)
+    standardizeFunc = lambda col: (col - np.nanmean(col))/np.nanstd(col)
+    out = None
+    uLabels = np.unique(labels)
+    for lab in uLabels:
+        ind = (labels == lab) & (~dropped)
+        tmpS = cyDf.iloc[:,ind].apply(standardizeFunc, raw = True).mean(axis = 1)
+        tmpS.name = 'M%s' % lab
+        if out is None:
+            out = pd.DataFrame(tmpS)
+        else:
+            out = out.join(tmpS)
+    return out
